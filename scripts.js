166485d8b9e7dae1546d2abba41795626c900e7d
Joao Vicente <112505296+joaorovera@users.noreply.github.com>
2023-09-20 10:59:49 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,40 +1,90 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
+const localStorageKey = 'to-do-list-gn'
 
-const jump = ( ) => {
- mario.classList.add('jump');
+function validateIfExistsNewTask() {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let inputValue = document.getElementById('input-new-task').value
+    let exists = values.find(x => x.name == inputValue)
+    return !exists ? false : true
+}
 
-    setTimeout (() => {
+function newTask() {
+    let input = document.getElementById('input-new-task')
+    input.style.border = ''
 
-        mario.classList.remove('jump');
+    if (!input.value) {
+        input.style.border = '1px solid red'
+        alert('Digite uma tarefa para adicionar em sua lista')
+    }
 
-    }, 500);
+    else if (validateIfExistsNewTask()) {
+        alert('Já existe uma tarefa com esse nome')
+    }
+    else {
+        let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+        values.push({
+            name: input.value,
+            deleted : false
+        })
+        localStorage.setItem(localStorageKey, JSON.stringify(values))
+        showValues()
+    }
+    input.value = ''
 
 }
 
-const loop = setInterval(()=>{
-
-    
-
-const pipePosition = pipe.offsetLeft;
-const marioPosition = window.getComputedStyle(mario).bottom.replace('px', '');
-
-if (pipePosition <= 120 && pipePosition > 0  && marioPosition < 80) {
-
-    pipe.style.animation = 'none';
-    pipe.style.left = `${pipePosition}px`;
-
-    pipe.style.animation = 'none';
-    mario.style.bottom = `${marioPosition}px`;
-
-    mario.src = './imagens/game-over.png';
-    mario.style.width = '75px'
-    mario.style.marginLeft = '45px'
-
-    clearInterval(loop);
+function showValues() {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let list = document.getElementById('to-do-list')
+    list.innerHTML = ''
+    for (let i = 0; i < values.length; i++) {
+        if (values[i].deleted) {
+            list.innerHTML += `
+            <li class="line-through">
+                ${values[i]['name']}
+                <div class="flex">
+                <button id='btn-ok' onclick='removeItem("${values[i].name}")'>
+                <i class="fa-solid fa-xmark icon-size"></i>
+            </button>
+                    <button id='btn-ok' onclick='riscarItem("${values[i]['name']}")'>
+                        <i class="fa-solid fa-arrow-rotate-left icon-size"></i>
+                    </button> 
+                </div>
+            </li>`
+        }   else {
+            list.innerHTML += `
+            <li>
+                ${values[i]['name']}
+                <div class="flex">
+                <button id='btn-ok' onclick='removeItem("${values[i].name}")'>
+    <i class="fa-solid fa-xmark icon-size"></i>
+</button>
+                <button id='btn-ok' onclick='riscarItem("${values[i]['name']}")'>
+                    <i class="fa fa-check icon-size"></i>
+                </button>
+                </div>
+            </li>`
+        }
+    }
 }
 
 
-},10);
+function riscarItem(data) {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let index = values.findIndex(x => x.name == data)
 
-document.addEventListener('keydown',jump);
+    values[index].deleted = !values[index].deleted
+    localStorage.setItem(localStorageKey, JSON.stringify(values))
+
+    showValues()
+}
+
+function removeItem(data) {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+    let index = values.findIndex(x => x.name == data);
+    values.splice(index, 1);
+    localStorage.setItem(localStorageKey, JSON.stringify(values));
+    showValues();
+}
+
+showValues()
+
